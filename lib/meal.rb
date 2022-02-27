c327bea1642378538ced4fbaa1ea0cec121fa84f
itsmattpaw <itsmattpaw@gmail.com>
2022-02-27 17:47:46 -0500

    commit tree 26e43822267f3f0771a3b756ac29ef1ac91436a3

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,16 @@
 class Meal
+    attr_accessor :total, :customer, :waiter, :tip
+    @@all = []
 
+    def initialize(waiter, customer, total, tip)
+        @waiter = waiter
+        @customer = customer
+        @total = total
+        @tip = tip
+        @@all << self
+    end
+    def self.all
+        @@all
+    end
+    
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,27 @@
 class Waiter
+    attr_accessor :name, :experience
+    @@all = []
+
+    def initialize(name, exp)
+        @name = name
+        @experience = exp
+        @@all << self
+    end
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip)
+        Meal.new(self, customer, total, tip)
+    end
+    def meals
+        Meal.all.select{|m| m.waiter == self}
+    end
+    def best_tipper
+        best = meals.max do |a, b|
+            a.tip <=> b.tip
+        end
+        best.customer
+    end
 
 end
